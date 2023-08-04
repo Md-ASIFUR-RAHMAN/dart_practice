add359837101886fcdca9e2a1cf2e3d4f9b1716e
mdasifurrahman <asifurr959@gmail.com>
2023-08-04 11:06:27 +0600

    Initial commit

---
+++ bin/dart_practice.dart
@@ -0,0 +1,21 @@
+void main(){
+
+
+  const a = 70;
+  const b = 30;
+
+  var Addition =  a + b;
+  var Subtraction =  a - b;
+  var Multiplication =  a * b;
+  var Division =  a/b;
+  var Modulus =  a % b;
+
+  print("a = $a" + "," + "b = $b");
+
+  print("Addition: $a + $b = $Addition");
+  print("Subtraction: $a - $b = $Subtraction");
+  print("Multiplication: $a * $b = $Multiplication");
+  print("Division: $a / $b = $Division");
+  print("Modulus: $a % $b = $Modulus");
+
+}
